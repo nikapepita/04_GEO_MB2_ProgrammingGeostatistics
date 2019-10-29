63a3c6712e52180987a259e741243ba14a73e53c
Annika Ludwig <s393305@uni-wuerzburg.eu>
2019-10-29 10:39:13 +0100

    First Commit

---
+++ Funpackages.R
@@ -0,0 +1,16 @@
+install.packages("fun")
+library(fun)
+if (.Platform$OS.type =="windows") x11() else x11(type="Xlib")
+mine_sweeper()
+
+install.packages("sudoku")
+library(sudoku)
+if (.Platform$OS.type =="windows") x11() else x11(type="Xlib") #need for 
+sudoku()
+
+install.packages("devtools")
+library(devtools)
+
+devtools::install_github("gaborcsadri/praise")
+library(praise)
+praise()
