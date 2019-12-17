ec48d65f2cafa4ebdc2fd57424edf306c3107cad
nikapepita <annika.ludwig@posteo.de>
2019-12-17 13:30:35 +0100

    .

--- Funpackages.R
+++ Funpackages.R
@@ -171,3 +171,105 @@ cbind.data.frame(ID = rep(1:N, TIMES)
   snow_plot
 
 snow_anim <- gganimate(snow_plot, nframes = TIMES, width = 600, height = 600)
+
+
+###flight routes 
+
+install.packages("pacman")
+library(pacman)
+
+p_load(data.table, geosphere, ggplot2, grid, jpeg, plyr, tidyverse)
+
+#Download Openflightsdata
+download.file("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat",
+              destfile="airlines.dat", mode="wb")
+
+download.file("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat",
+              destfile="airports.dat", mode="wb")
+
+download.file("https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat",
+              destfile="routes.dat", mode="wb")
+
+#Import data
+airlines <- fread("airlines.dat",sep=",",skip=1)
+airports <- fread("airports.dat", sep=",")
+routes <- fread("routes.dat", sep=",")
+
+# Add column names
+colnames(airlines) <- c("airline_id", "name", "alias", "iata", "icao", "callsign", "country", "active")
+colnames(airports) <- c("airport_id", "name", "city", "country", "iata", "icao", "latitude", "longitude", 
+                        "altitude", "timezone", "dst", "tz_database_time_zone", "type", "source")
+colnames(routes) <- c("airline", "airline_id", "source_airport", "source_airport_id", "destination_airport", 
+                      "destination_airport_id", "codeshare", "stops", "equipment")
+
+# Convert character to numeric
+routes$airline_id <- as.numeric(routes$airline_id)
+
+# Merge airline data with data on routes
+flights <- left_join(routes, airlines, by = "airline_id")
+
+# Merge data on flights with information on airports
+airports_orig <- airports[, c(5, 7, 8)]
+colnames(airports_orig) <- c("source_airport", "source_airport_lat", "source_airport_long")
+
+airports_dest <- airports[, c(5, 7, 8)]
+colnames(airports_dest) <- c("destination_airport", "destination_airport_lat", "destination_airport_long")
+
+flights <- left_join(flights, airports_orig, by = "source_airport")
+flights <- left_join(flights, airports_dest, by = "destination_airport")
+
+# Remove observations with missing values
+flights <- na.omit(flights, cols = c("source_airport_long", "source_airport_lat", "destination_airport_long", "destination_airport_lat"))
+
+# Split the data into separate data sets
+flights_split <- split(flights, flights$name)
+
+# Calculate intermediate points between each two locations
+flights_all <- lapply(flights_split, function(x) gcIntermediate(x[, c("source_airport_long", "source_airport_lat")], 
+                                                                x[, c("destination_airport_long", "destination_airport_lat")], 
+                                                                100, breakAtDateLine = FALSE, addStartEnd = TRUE, sp = TRUE))
+
+# Turn data into a data frame for mapping with ggplot2
+flights_fortified <- lapply(flights_all, function(x) ldply(x@lines, fortify))
+
+# Unsplit lists
+flights_fortified <- do.call("rbind", flights_fortified)
+
+# Add and clean column with airline names
+flights_fortified$name <- rownames(flights_fortified)
+flights_fortified$name <- gsub("\\..*", "", flights_fortified$name)
+
+# Extract first and last observations for plotting source and destination points (i.e., airports)
+flights_points <- flights_fortified %>%
+  group_by(group) %>%
+  filter(row_number() == 1 | row_number() == n())
+
+# Download NASA night lights image
+download.file("https://www.nasa.gov/specials/blackmarble/2016/globalmaps/BlackMarble_2016_01deg.jpg", 
+              destfile = "BlackMarble_2016_01deg.jpg", mode = "wb")
+
+# Load picture and render
+earth <- readJPEG("BlackMarble_2016_01deg.jpg", native = TRUE)
+earth <- rasterGrob(earth, interpolate = TRUE)
+
+ggplot() +
+  annotation_custom(earth, xmin = -180, xmax = 180, ymin = -90, ymax = 90) +
+  geom_path(aes(long, lat, group = id, color = name), alpha = 0.0, size = 0.0, data = flights_fortified) + 
+  geom_path(aes(long, lat, group = id, color = name), alpha = 0.2, size = 0.3, color = "#f9ba00", data = flights_fortified[flights_fortified$name == "Lufthansa", ]) + 
+  geom_point(data = flights_points[flights_points$name == "Lufthansa", ], aes(long, lat), alpha = 0.8, size = 0.1, colour = "white") +
+  theme(panel.background = element_rect(fill = "#05050f", colour = "#05050f"), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        axis.title = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks.length = unit(0, "cm"),
+        legend.position = "none") +
+  annotate("text", x = -150, y = -18, hjust = 0, size = 14,
+           label = paste("Lufthansa"), color = "#f9ba00", family = "Helvetica Black") +
+  annotate("text", x = -150, y = -26, hjust = 0, size = 8, 
+           label = paste("Flight routes"), color = "white") +
+  annotate("text", x = -150, y = -30, hjust = 0, size = 7, 
+           label = paste("lhehnke.github.io || NASA.gov || OpenFlights.org"), color = "white", alpha = 0.5) +
+  coord_equal() 
+
+ggsave("Lufthansa.png", width = 36, height = 18, units = "in", dpi = 100)
